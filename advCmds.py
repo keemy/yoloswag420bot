5c39b7d2d45ddd66a3a0faec15b8753cc4e9c295
devin c-r <devin1891@gmail.com>
2014-07-12 22:27:42 +0000

    updating gitignore

--- advCmds.py
+++ advCmds.py
@@ -49,3 +49,14 @@ def currentMasteries():
     return cur["name"] + " - " + "/".join([str(i) for i in dist])
 advCmds["!masteries"] = currentMasteries
 
+def skinCode(a=[-1]):
+#    codes = ["RWWNH74XF3DUKM","RW6DGGL96FUGQ6","RWWP77FD3JD64A","RWTCH9HDVCNUYX","RW9QJUFCXPCXG2","RW6ME4KV49LURC","WWPPL4D3K4P3AM","WW93CDWU9TFNWW","WWGQC4LT346AWM","MYGGEG6P9AM2","YQG273CXKUPF","RTHGGXWPAQ62","E6A6V47EUCHF","NF72KKGXXUAF","HU3LUFAGLU96","CRL2YLJVDQTF","MXWYYGDU9J4R"]
+    codes = []
+    a[0]+=1
+
+    if a[0] < len(codes):
+        return codes[a[0]]
+    else:
+        return "No more codes kids"
+
+advCmds["!skincode"]=skinCode
